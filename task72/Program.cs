bc7f2c8aa81d9a4de81f35625b97bce7a8544545
Dmitry Mordanov <Mordanov314@gmail.com>
2022-04-16 01:25:28 +0300

    first and final commit

---
+++ task72/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 72: Заданы 2 массива: info и data. В массиве info 
+// хранятся двоичные представления нескольких чисел (без разделителя). 
+// В массиве data хранится информация о количестве бит, которые занимают числа из массива info. 
+// Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом 
+// информации из массива info.
+
+// Пример:
+// входные данные:
+
+// data = {0, 1, 1, 1, 1, 0, 0, 0, 1 } info = {2, 3, 3, 1 }
+
+// выходные данные:1, 7, 0, 1
+
+// Комментарий: первое число занимает 2 бита (01 -> 1); второе число занимает 3 бита (111 -> 7); 
+// третье число занимает 3 бита (000 -> 0; четвёртое число занимает 1 бит (1 -> 1)
+
+using System;
+
+void PrintArray(int [] local_array)  // красивая печать одномерного массива
+{
+  Console.Write("[");
+  for(int i=0;i<local_array.Length - 1;i++)
+  {
+      Console.Write($"{local_array[i]:d}; ");
+  }
+  Console.WriteLine($"{local_array[local_array.Length - 1 ]:d}]");
+}
+
+
+const int size_info = 10; // Столько чисел будет в результате
+
+int [] info = new int [size_info];  
+
+Random rnd = new Random();
+int size_data = 0;
+
+for (int i = 0; i < info.Length; i++)  // 
+{
+    info[i] = rnd.Next(1,5); // числа будут от 1 до 4 бит
+    size_data += info[i];
+}
+
+int [] data = new int[size_data];
+
+for (int i = 0; i < data.Length; i++)  // заполняем массив с данными
+{
+    data[i] = rnd.Next(0,2);
+}
+
+int [] ProcessArray()  // возвращаем 10-чный массив в соответствии с info[] по data[]
+{
+    int [] ret = new int [info.Length];
+    int j = 0;  // тут храним позицию, с которой начинается текущее число в data[]
+    for (int i = 0; i < info.Length; i++)
+    {
+        for (int k=0; k < info[i]; k++)
+        {
+            ret[i] += (int)Math.Pow(2,k)*data[j+info[i]-k-1];   // степень двойки умножаем на текущий бит 
+        }                                                       // в обратном порядке
+        j += info[i];
+    }
+    return ret;
+}
+
+Console.WriteLine("Массив с данными:");
+PrintArray(data);
+Console.WriteLine("Массив с информацией:");
+PrintArray(info);
+int [] rezult = ProcessArray();
+Console.WriteLine("Итоговый результат:");
+PrintArray(rezult);
---
+++ task73/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 73: Есть число N. Сколько групп M, можно получить при разбиении всех чисел на группы,
+// так чтобы в одной группе все числа были взаимно просты (все числа в группе друг на друга не делятся)?
+// Найдите M при заданном N и получите одно из разбиений на группы N <= 10^20.
+
+// Например, для N = 50, M получается 6
+// Группа 1: 1
+// Группа 2: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+// Группа 3: 4 6 9 10 14 15 21 22 25 26 33 34 35 38 39 46 49
+// Группа 4: 8 12 18 20 27 28 30 42 44 45 50
+// Группа 5: 16 24 36 40
+// Группа 6: 32 48
+
+using System;
+
+void MyPrintMatrix(int [,] localMatrix)  // красиво печатаем матрицу
+{
+    for (int i=0; i<localMatrix.GetLength(0);i++)
+    {
+        if (localMatrix[i,0] == 0) {break;} // обрываем печать строки
+        else
+        {
+            Console.Write($"Группа {i+1}: ");
+            for (int j=0; j<localMatrix.GetLength(1);j++)
+            {
+                if (localMatrix[i,j] == 0) {break;}
+                else
+                    Console.Write($"{localMatrix[i,j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+void ProcessMatrix(int n, int [,] localMatrix)  // делаем расчет матрицы
+{
+    bool flag = false;  
+    for (int k = 1; k<=n; k++)  // цикл по всем числам
+    {
+        flag = false;
+        for (int i = 0; i<localMatrix.GetLength(0); i++)  // цикл по строкам
+        {
+            for (int j = 0; j<localMatrix.GetLength(1); j++)
+            {
+                if (localMatrix[i,j] == 0)  // если нашли пустую ячеку - то заполняем ее 
+                {                           // и выходим из циклов i и j
+                    localMatrix[i,j]=k;
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                   if (k % localMatrix[i,j] == 0) {break;}  // если число делится на то, что есть в матрице
+                                                            // то переходим в следующую группу
+                }
+            }
+            if (flag) {break;}  // это если заполнили свободную ячейку - чтобы перейти к след. числу
+        }
+    }
+}
+
+// Основная программа
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// заполняем матрицу нулями
+int [,] matrix = new int [N,N];
+for (int i = 0; i<matrix.GetLength(0); i++)
+    for (int j = 0; j<matrix.GetLength(1); j++)
+        matrix[i,j] = 0;
+
+ProcessMatrix(N,matrix);  // расчет
+
+int M = 0;  // считаем заполненные строки в матрице
+for (int i = 0;i<matrix.GetLength(0); i++)
+    if (matrix[i,0] == 0) {break;}
+    else {M++;}
+
+Console.WriteLine($"Для N={N}, будет M={M} взаимно простых групп:");
+
+MyPrintMatrix(matrix);
